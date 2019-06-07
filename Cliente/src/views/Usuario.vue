56acbc7e4e555c3404b3b8be5725e5d6de5d7163
Geo <geeox96@gmail.com>
2019-06-07 16:40:07 -0300

    termino de criação de usuario, time alert

--- Cliente/src/views/Usuario.vue
+++ Cliente/src/views/Usuario.vue
@@ -2,44 +2,79 @@
   <v-app id="inspire">
     <v-layout row>
       <v-flex>
+          <v-alert
+            dismissible
+            :value="false"
+            v-model="notificacaoCaixaFechada"
+            type="success"
+          >Você clicou em cancelar</v-alert></v-flex>
+      <v-flex>
         <v-card>
+          
           <v-toolbar color="#023788" dark>
             <v-toolbar-title>Usuarios</v-toolbar-title>
-<v-spacer></v-spacer>
-
-            <v-dialog v-model="caixaNovoUsuario" max-width="40%">
+            <v-spacer></v-spacer>
+            <v-dialog v-model="caixaNovoUsuario" max-width="800px">
               <template v-slot:activator="{ on }">
                 <v-icon v-on="on">add</v-icon>
               </template>
               <v-card>
                 <v-card-title>
                   <v-spacer></v-spacer>
+
                   <span class="headline">Novo Usuario</span>
                   <v-spacer></v-spacer>
                 </v-card-title>
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12>
-                        <v-text-field :rules="[validacao.obrigatorio]" v-model="dadosUsuario.nome" label="Nome Completo" placeholder="João Mariano Freitas"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12>
-                        <v-text-field :rules="[validacao.obrigatorio, validacao.email]" v-model="dadosUsuario.email" label="E-Mail" placeholder="exemplo@servidor.com"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6>
-                        <v-text-field :rules="[validacao.obrigatorio]" :append-icon="mostrarSenha ? 'visibility' : 'visibility_off'" :type="mostrarSenha ? 'text' : 'password'" v-model="dadosUsuario.senha" label="Senha" @click:append="mostrarSenha = !mostrarSenha"></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6>
-                        <v-text-field :rules="[validacao.obrigatorio, validacao.min]" :append-icon="mostrarSenha1 ? 'visibility' : 'visibility_off'" :type="mostrarSenha1 ? 'text' : 'password'" v-model="dadosUsuario.senhaConfirmacao" label="Repita a senha" @click:append="mostrarSenha1 = !mostrarSenha1"></v-text-field>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-card-text>
 
+                <v-form ref="form" v-model="validade" lazy-validation>
+                  <v-card-text>
+                    <v-container grid-list-md>
+                      <v-layout wrap>
+                        <v-flex xs12>
+                          <v-text-field
+                            :rules="[validacao.obrigatorio]"
+                            v-model="dadosUsuario.nome"
+                            label="Nome Completo"
+                            placeholder="João Mariano Freitas"
+                          ></v-text-field>
+                        </v-flex>
+
+                        <v-flex xs12>
+                          <v-text-field
+                            :rules="[validacao.obrigatorio, validacao.email]"
+                            v-model="dadosUsuario.email"
+                            label="E-Mail"
+                            placeholder="exemplo@servidor.com"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6>
+                          <v-text-field
+                            :rules="[validacao.obrigatorio, validacao.min]"
+                            :append-icon="mostrarSenha ? 'visibility' : 'visibility_off'"
+                            :type="mostrarSenha ? 'text' : 'password'"
+                            v-model="dadosUsuario.senha"
+                            label="Senha"
+                            @click:append="mostrarSenha = !mostrarSenha"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6>
+                          <v-text-field
+                            :rules="[validacao.obrigatorio, validacao.min, validacao.senhaValidacao]"
+                            :append-icon="mostrarSenha1 ? 'visibility' : 'visibility_off'"
+                            :type="mostrarSenha1 ? 'text' : 'password'"
+                            v-model="dadosUsuario.senhaConfirmacao"
+                            label="Repita a senha"
+                            @click:append="mostrarSenha1 = !mostrarSenha1"
+                          ></v-text-field>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                  </v-card-text>
+                </v-form>
                 <v-card-actions>
                   <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" flat @click="fechar">Cencelar</v-btn>
-                  <v-btn color="blue darken-1" flat @click="salvar">Criar</v-btn>
+                  <v-btn @click="fechar">Cencelar</v-btn>
+                  <v-btn :disabled="!validade" @click="salvar">Criar</v-btn>
                 </v-card-actions>
               </v-card>
             </v-dialog>
@@ -55,7 +90,7 @@
               <td>{{ props.item.nome }}</td>
               <td class="text-right">{{ props.item.email }}</td>
               <td class="justify-center layout px-0">
-                <v-icon color="#023788"  medium class="mr-2" @click="editarItem(props.item)">edit</v-icon>
+                <v-icon color="#023788" medium class="mr-2" @click="editarItem(props.item)">edit</v-icon>
               </td>
             </template>
           </v-data-table>
@@ -80,10 +115,13 @@ export default {
       mostrarSenha: false,
       mostrarSenha1: false,
       caixaNovoUsuario: false,
+      notificacaoCadastrado: false,
+      notificacaoCaixaFechada: false,
+      validade: false,
       usuarios: [],
       itens: [
-        { text: "Nome", value: "nome", sortable: false },
-        { text: "E-mail", value: "email", sortable: false },
+        { text: "Nome", value: "nome" },
+        { text: "E-mail", value: "email" },
         { text: "Opções", value: "name", sortable: false }
       ],
       dadosUsuario: {
@@ -99,59 +137,54 @@ export default {
         senhaConfirmacao: ""
       },
       validacao: {
-        obrigatorio: value => !!value || 'Campo Obrigatorio',
-        min: v => v.length >= 6 || 'Campo deve ter no minimo 6 digitos',
+        obrigatorio: value => !!value || "Campo Obrigatorio",
+        min: value => value.length >= 6 || "Campo deve ter no minimo 6 digitos",
         email: value => {
-          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-          return pattern.test(value) || 'E-mail Invalido'
+          const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+          return pattern.test(value) || "E-mail Invalido";
         },
-        emailMatch: () => ('The email and password you entered don\'t match')
-      }
+        senhaValidacao: value =>
+          value == this.dadosUsuario.senha || "As senhas não coincidem"
+      },
     };
   },
 
   computed: {
-    regraSenhaConfirmada() {
-      const rule =
-          v => (!!v && v) === this.match ||
-            'Values do not match'
-
-        rules.push(rule)
-    }
+    
   },
 
-
   methods: {
-
-    obterUsuarios () {
+    obterUsuarios() {
       Vue.axios.get(apiUrl + "usuario").then(response => {
         this.usuarios = response.data;
-      })
+      });
     },
 
-    cadastrarUsuario () {
-      Vue.axios.post(apiUrl + "usuario",
-      {'nome': this.dadosUsuario.nome,
-       'email': this.dadosUsuario.email,
-      'senha': this.dadosUsuario.senha})
+    cadastrarUsuario() {
+      Vue.axios.post(apiUrl + "usuario", {
+          nome: this.dadosUsuario.nome,
+          email: this.dadosUsuario.email,
+          senha: this.dadosUsuario.senha
+        })
+        .then(response => {
+          this.notificacaoCadastrado = true;
+        });
     },
 
-    validarConfirmacao () {
-      this.$refs.form.validate()
+    fechar() {
+      this.dadosUsuario = Object.assign({}, this.dadosLimpo);
+      this.caixaNovoUsuario = false;
+      this.notificacaoCaixaFechada = true;
     },
 
-    fechar () {
-      this.caixaNovoUsuario = false
-    },
-
-    salvar () {
-      if( this.dadosUsuario.senha == this.dadosUsuario.senhaConfirmacao){
-        this.cadastrarUsuario()
-        this.dadosUsuario = Object.assign({}, this.dadosLimpo)
+    salvar() {
+      if (this.$refs.form.validate()) {
+        this.cadastrarUsuario();
+        this.obterUsuarios();
+        this.dadosUsuario = Object.assign({}, this.dadosLimpo);
+        this.fechar();
       }
-        this.fechar()
-    }
-
+    },
   },
 
   mounted() {
